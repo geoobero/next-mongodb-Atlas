ba5d19d6a5b9b09173a97684970f0188525acb27
Geo <geoobero@gmail.com>
2026-04-14 23:01:13 +0800

    modal update

--- app/profile/page.js
+++ app/profile/page.js
@@ -148,7 +148,6 @@ export default function Profile() {
                 </div>
                 <h3 className="text-xl font-semibold text-white mb-2">Success!</h3>
                 <p className="text-white/60 mb-4">Profile updated successfully!</p>
-                <p className="text-white/40 text-sm">This modal will close automatically</p>
               </div>
             </div>
           </div>
